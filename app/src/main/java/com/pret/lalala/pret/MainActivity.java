bef79366fdd10831337bd6f984022d05f1edcd46
muhamistau <islam.taufikurahman@gmail.com>
2018-11-18 15:17:45 +0700

    Menyambungkan Fragments dengan Bottom Navigation

--- app/src/main/java/com/pret/lalala/pret/MainActivity.java
+++ app/src/main/java/com/pret/lalala/pret/MainActivity.java
@@ -1,13 +1,68 @@
 package com.pret.lalala.pret;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    fragment = new FragmentHome();
+                    break;
+
+                case R.id.action_bookmarks:
+                    fragment = new FragmentFavorit();
+                    break;
+
+                case R.id.action_add:
+                    fragment = new FragmentAdd();
+                    break;
+
+                case R.id.action_messages:
+                    fragment = new FragmentMessages();
+                    break;
+
+                case R.id.action_user:
+                    fragment = new FragmentProfile();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+
+        private boolean loadFragment(Fragment fragment) {
+            //switching fragment
+            if (fragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
